25f5c205ac0b0ea8c75bef2cbc17fa33980db46b
SeungYeon <orrhrr@gmail.com>
2024-02-09 13:39:24 +0900

    feat: 선호장르 선택 페이지 조립

---
+++ src/components/layout/settingPageLayout.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+
+interface SettingPageLayoutProps {
+  header: ReactNode;
+  main: ReactNode;
+}
+
+function SettingPageLayout({ header, main }: SettingPageLayoutProps) {
+  return (
+    <>
+      <div role="container" className="flex flex-col gap-40">
+        <div role="header">{header}</div>
+        <div role="content" className="flex-center">
+          {main}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SettingPageLayout;
