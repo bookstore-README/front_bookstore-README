3a8b1f7b035384fe825b4889123148f1e5efa29e
SeungYeon <orrhrr@gmail.com>
2024-02-10 13:38:51 +0900

    style: 컴포넌트명 수정

--- src/pages/mypage/review/index.tsx
+++ src/pages/mypage/review/index.tsx
@@ -2,8 +2,8 @@ import MyReviewCardList from '@/components/card/bookReviewCard/myReviewCardList'
 import MainLayout from '@/components/layout/mainLayout';
 import { myReviews } from '@/pages/api/mock/myReviewMock';
 
-export default function MyCommunityPage() {
-  // TODO: 커뮤니티 데이터 서버에서 받아온 뒤 mock 데이터 교환
+export default function MyReviewPage() {
+  // TODO: 내리뷰 데이터 서버에서 받아온 뒤 mock 데이터 교환
   return (
     <MainLayout>
       <div className="mx-auto flex w-full flex-col justify-center px-60 py-20 mobile:px-15 mobile:py-0 tablet:px-40">
