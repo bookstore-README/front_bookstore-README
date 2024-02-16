d363d3c452186d5515b189c12df44134dab64c72
SeungYeon <orrhrr@gmail.com>
2024-02-16 18:26:48 +0900

    refactor: 모든layout 컴포넌트를 mainLayout으로 감싸기

--- src/components/layout/settingPageLayout.tsx
+++ src/components/layout/settingPageLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import ScrollToTopButton from '../button/scrollToTopButton';
+import MainLayout from './mainLayout';
 
 interface SettingPageLayoutProps {
   header: ReactNode;
@@ -13,13 +14,12 @@ function SettingPageLayout({
   isTopButton,
 }: SettingPageLayoutProps) {
   return (
-    <div role="container" className="flex flex-col gap-40">
-      <div role="header">{header}</div>
+    <MainLayout>
       <div role="content" className="flex-center pb-40">
         {main}
       </div>
       {isTopButton && <ScrollToTopButton />}
-    </div>
+    </MainLayout>
   );
 }
 
