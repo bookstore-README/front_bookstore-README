de8953f71dc1255f7051402c576013f3a6c238f5
SeungYeon <orrhrr@gmail.com>
2024-02-20 08:58:15 +0900

    임시저장

--- src/pages/mypage/order/index.tsx
+++ src/pages/mypage/order/index.tsx
@@ -6,12 +6,18 @@ import {
   bookOrderTestData,
   orderOverViewData,
 } from '@/pages/api/mock/bookOrderMock';
+import OrderDate from '@/components/container/orderDate/orderDate';
+import { useState } from 'react';
 const { orderData } = bookOrderTestData;
 
 function MyOrderPage() {
+  const [startDate, setStartDate] = useState<Date>();
+  const [endDate, setEndDate] = useState<Date>();
+  const [selectedItem, setSelectedItem] = useState('전체보기');
+  const onSelectedItem = (menu: string) => setSelectedItem(menu);
   return (
     <MyOrderPageLayout
-      // orderDate={<OrderDate />}
+      orderDate={<OrderDate />}
       overview={<OrderOverView orderView={orderOverViewData.orderView} />}
       main={
         orderData ? (
