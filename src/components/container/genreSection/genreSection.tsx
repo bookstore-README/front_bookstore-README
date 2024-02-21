8c7237035807b2d4049d2dae6fe6bbc837637572
SeungYeon <orrhrr@gmail.com>
2024-02-21 17:43:33 +0900

    feat: 선택된 카테고리 가져와서 UI처리

--- src/components/container/genreSection/genreSection.tsx
+++ src/components/container/genreSection/genreSection.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GenreButton from '@/components/button/genre/genreButton';
 import { MemberCategoryType, ReadMeGenreList } from '@/pages/api/mock';
 import EditToggleButton from '@/components/button/editToggleButton';
 import { notify } from '@/components/toast/toast';
 import { useGetCustomCategoryList } from '@/api/category';
+import { useAtom } from 'jotai';
+import { CategoryListAtom } from '@/store/state';
 interface CustomCategoryListResponse {
   data?: {
     data: {
@@ -15,17 +17,20 @@ interface CustomCategoryListResponse {
 function GenreSection() {
   const [isEditMode, setEditMode] = useState(false);
   const [isSelected, setIsSelected] = useState(false);
+  const [categoryList] = useAtom(CategoryListAtom);
   const genres = ReadMeGenreList.genreList;
+
+  // 유저가 선택한 카테고리 리스트 가져오기
   const response: CustomCategoryListResponse = useGetCustomCategoryList();
   let memberCategory: MemberCategoryType[] = [];
   if (response.data && response.data.data) {
     memberCategory = response.data.data.memberCategory;
   }
-  const selectedGenre = memberCategory.map(
-    (selectedGenre) => selectedGenre.subName,
+
+  // 유저가 선택한 카테고리의 subId 리스트
+  const selectedGenreId = memberCategory.map(
+    (selectedGenre) => selectedGenre.subId,
   );
-  console.log(selectedGenre);
-  // const selectedGenre = memberCategory[index].subId === genre.title;
 
   const handleEditModeToggle = () => {
     setEditMode((prev) => !prev);
@@ -43,16 +48,25 @@ function GenreSection() {
     return 'flex-center flex-wrap pc:w-[1028px] tablet:w-[688px] mobile:w-331 gap-4';
   };
 
+  useEffect(() => {
+    if (response.data && response.data.data) {
+      memberCategory = response.data.data.memberCategory;
+    }
+  });
+
   return (
     <div className="flex-center flex-col">
       <div className="mb-28 text-20 font-bold">선호장르 선택</div>
 
       <div className={`${getButtonLayoutClass()}`}>
-        {genres.map((genre, index) => (
+        {categoryList?.domestic.map((category, index) => (
           <GenreButton
             key={index}
-            title={genre.title}
-            selected={selectedGenre === genre.title}
+            title={category.subName ?? ''}
+            selected={
+              category?.subId !== undefined &&
+              selectedGenreId.includes(category.subId)
+            }
             editMode={isEditMode}
           />
         ))}
